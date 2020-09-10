2be2e31c01bf107baffe3af720df752aaf393451
Vitalii Protsenko <vp412@ic.ac.uk>
2020-09-10 13:01:37 +0100

    create stubs for submit_sim api endpoint

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^report/<uuid:report_id>/', views.report, name='report'),
+    url(r'^submit-sim/', views.submit_sim, name='submit_sim'),
     url(r'^$', views.index, name='index'),
 ]
--- web/views.py
+++ web/views.py
@@ -9,3 +9,21 @@ def index(request):
     ctx = {'text': ' there xD'}
 
     return render(request, 'index.html', ctx)
+
+
+@require_POST
+def submit_sim(request):
+    engine = Engine('''TODO params here''')
+    sims_report = engine.simulate()
+    request.session['report_id'] = sims_report['report_id']
+    return redirect(report)
+
+
+@require_GET
+def report(request):
+    ctx = {
+
+    }
+    report_id = request.session['report_id']
+    # TODO get report from db with report_id and display it, along with input data
+    return render(request, 'report.html', ctx)
